668ddbca8409a87028b133cba1b8dd21df6d907f
Mathis Logemann <mathisloge@gmail.com>
2021-09-17 10:48:25 +0200

    add cpp-json-schema

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # vcpkg-registry
 
+Custom vcpkg registry. Contains my own libraries which are just used for own projects and ports that are not maintained by upstream and therefore not suited for vcpkg per policy.
+
 Currently included packages:
 
+* cpp-json-schema
 * lua51 => lua 5.1.3
 * lua52 => lua 5.2.4
 * lua53 => lua 5.3.6
 * turf
 * junction
-
 
 # usage
 add the following to your `vcpkg-configuration.json`. For a more detailed explanation see https://github.com/microsoft/vcpkg/blob/master/docs/specifications/registries-2.md#consuming-registries
---
+++ ports/cpp-json-schema/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mathisloge/cpp-json-schema
+    REF 14263be4d066a3111453d137f9d1cf531ed9c423
+    SHA512 fce2678da87967677392e6c1e0af241769acb98ca25616062597e3fa7606633e620d0c09a5f82676894d91a033c5646a2b921f6df252c0fa4215684684c8d081
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
